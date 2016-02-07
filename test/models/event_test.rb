3effb3fa874e8df687b1a0e483c0d75b29c0c59e
oookli <oklika@gmail.com>
2016-02-07 15:42:09 +0200

    create simple test, create fixtures

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "start date should not be equal end date" do
+    event_1 = events(:event_one)
+    event_2 = events(:event_two)
+    assert_not_equal event_1.start_date, event_1.end_date
+    assert_not_equal event_2.start_date, event_2.end_date
+  end
 end
