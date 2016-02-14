b9786faddc0904db80c8ae48672f4752a1cd750f
oookli <oklika@gmail.com>
2016-02-14 16:32:43 +0200

    add status to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214125635) do
+ActiveRecord::Schema.define(version: 20160214125812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160214125635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "project_id"
+    t.string   "status"
   end
 
   create_table "projects", force: :cascade do |t|
