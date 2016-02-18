44f703578b21b7a7ecd470eda605c9dcadcdf3f5
marko <i.markozub@gmail.com>
2016-02-18 13:54:10 -0500

    VF-05: updated projects views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-  	@projects = Project.limit(3)
+  	@projects = Project.limit(3).order(:created_at)
 
   	# Volounteers !where user have status volounteer or joined to the Project
   	@volounteers = User.limit(8)
